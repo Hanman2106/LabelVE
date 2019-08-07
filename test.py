c9659f00fb5bcd5eeb3eadc1c02656ee28f4f2e9
BHan <BerhanHan89@googlemail.com>
2019-08-07 15:26:22 +0200

    main update Portabfrage und Dosenabfrage

--- test.py
+++ test.py
@@ -20,19 +20,16 @@ while i <= vt:
 	j = 0
 	while j < p:
 		k = 1
-		ports = int(input("Anzahl der ports des Panels %s angeben: " %a[j]))
-		dosen = (input("Handelt es sich bei den Ports des Panels %s um Doppeldosen? (ja/nein): " %a[j]))		
+		ports = int(input("Anzahl der ports des Panels %s angeben: " %a[j]))	
 		while k <= ports:
-			if dosen == 'ja':
-				string = '%s%i VT%i %s%i'% (a[j], k, i, a[j], k+1)
-				k = k + 2
-			elif dosen == 'nein':
-				string = 'VT%i %s%i'% (i, a[j], k)
-				k = k + 1
+			string = '%s%i VT%i %s%i'% (a[j], k, i, a[j], k+1)
 			print(string)
 			writer.writerow({'A' : string})
+			k = k + 2
 		j = j + 1
 	i = i + 1
 
 gcsv.close()
+
+
 
