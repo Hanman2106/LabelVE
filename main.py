73229274b97db274f510dd68b5310b197c969db6
BHan <BerhanHan89@googlemail.com>
2019-08-07 14:34:35 +0200

    test.py erstellt standard Label

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import requests
+import csv
+import kivy
+kivy.require('1.11.1')
 
-response = requests.get('https://httpbin.org/ip')
+from kivy.app import App
+from kivy.uix.widget import Widget
 
-print('Your IP is {0}'.format(response.json()['origin']))
+
+class label(Widget):
+    pass
+
+class labelApp(App):
+
+    def build(self):
+        return label()
+
+
+if __name__ == '__main__':
+    labelApp().run()
+
+
+
+
+
