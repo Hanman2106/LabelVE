76269d1d3636fe6592ef5cf95d0c8a327887d7a7
BHan <BerhanHan89@googlemail.com>
2019-08-14 16:10:32 +0200

    ZS

--- gui.py
+++ gui.py
@@ -1,38 +1,80 @@
 import csv
 from PyQt5.QtWidgets import *
-
-
+from datetime import datetime
+import string
 import sys
 
-class Dialog(QDialog):
-    NumGridRows = 3
-    NumButtons = 4
+ts = datetime.fromtimestamp(datetime.now().timestamp()).isoformat()
 
-    def __init__(self):
-        super(Dialog, self).__init__()
-        self.createFormGroupBox()
-        
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        buttonBox.accepted.connect(self.accept)
-        buttonBox.rejected.connect(self.reject)
-        
-        mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.formGroupBox)
-        mainLayout.addWidget(buttonBox)
-        self.setLayout(mainLayout)
-        
-        self.setWindowTitle("Label Vorlagen Ersteller")
-        
-    def createFormGroupBox(self):
-        self.formGroupBox = QGroupBox("Form layout")
-        layout = QFormLayout()
-        layout.addRow(QLabel("Anzahl Verteiler:"), QLineEdit())
-        layout.addRow(QLabel("Panele:"), QLineEdit())
-        layout.addRow(QLabel("Ports:"), QLineEdit('24'))
-        self.formGroupBox.setLayout(layout)
+gcsv = open('Glabel-CSV-%s.csv' %ts, "w+")
+fnames = ['A']
+writer = csv.DictWriter(gcsv,fieldnames=fnames)
+writer.writeheader
+
+a = string.ascii_uppercase
+
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.vt = QLineEdit("Anzahl der Vereiler hier eintragen")
+        self.button = QPushButton("Bestätigen")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.vt)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.panele)
+	
+    def panele(self):
+        i = 1
+        while i <= int(self.vt.text()):
+            self.pan = QLineEdit("Anzahl der Panele für VT%i hier eintragen" %i)
+            self.button = QPushButton("Bestätigen")
+            layout = QVBoxLayout()
+            layout.addWidget(self.pan)
+            layout.addWidget(self.button)
+            self.setLayout(layout)
+            self.button.clicked.connect(self.ports)
+            i = i + 1
+
+    def ports(self):
+	    j = 0
+	    while j < self.pan.text():
+		    k = 1
+		    self.ports = QLineEdit("24")
+		    self.dosen = QLineEdit("J")
+		    self.button = QPushButton("Bestätigen")
+		    layout = QVBoxLayout()
+		    layout.addWidget(self.ports)
+		    layout.addWidget(self.dosen)
+		    layout.addWidget(self.button)
+		    # Set dialog layout
+		    self.setLayout(layout)
+		    # Add button signal to greetings slot
+		    self.button.clicked.connect(self.schleifen)
+		    j = j + 1
+
+    def schleifen(self):
+        while k <= self.ports.text():
+            if self.dosen.text() == 'J':
+                string = '%s%i VT%i %s%i'% (a[j], k, i, a[j], k+1)
+                k = k + 2
+            elif self.dosen.text() == 'n':
+                string = 'VT%i %s%i'% (i, a[j], k)
+                k = k + 1
+            print(string)
+            writer.writerow({'A' : string})
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	dialog = Dialog()	
-	sys.exit(dialog.exec_())
-
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
